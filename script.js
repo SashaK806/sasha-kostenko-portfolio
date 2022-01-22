8e38a2d67e8ebc7555617d19c02e4de7b71090bd
SashaK806 <54603786+SashaK806@users.noreply.github.com>
2022-01-22 14:35:37 +0100

    updated

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("work").addEventListener("mouseover", imgMouseOver);
+
+function imgMouseOver() {
+    document.getElementsByClassName("name").style.visibility = "visible";
+}
