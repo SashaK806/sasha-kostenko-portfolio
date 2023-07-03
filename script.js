e7b5256e55740ac9aa97aaac2988cf24929ccd38
SashaK806 <54603786+SashaK806@users.noreply.github.com>
2023-07-03 17:22:05 -0500

    javascript initial

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.getElementsByClassName("work").addEventListener("mouseover", imgMouseOver);
+// document.getElementsByClassName("work").addEventListener("mouseover", imgMouseOver);
+document.getElementsByClassName("sparkle").addEventListener("mouseover", sparkle);
 
-function imgMouseOver() {
-    document.getElementsByClassName("name").style.visibility = "visible";
+// function imgMouseOver() {
+//     document.getElementsByClassName("name").style.visibility = "visible";
+// }
+
+function sparkle() {
+    const letter = document.getElementsByClassName("sparkle");
+    const sparkle = "*";
+    letter.replaceWith(sparkle);
 }
