8c67fb437513d2643b83f73a87509d7bfd29da25
SashaK806 <54603786+SashaK806@users.noreply.github.com>
2024-02-27 18:55:19 -0600

    project and description refresh
    updated number of projects and what's shown on home page. Added more detailed captions under process images. Updated contact information. updated About Me.

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-// document.getElementsByClassName("work").addEventListener("mouseover", imgMouseOver);
-document.getElementsByClassName("sparkle").addEventListener("mouseover", sparkle);
+let mainContent = document.querySelector('.column');
+let secondaryContent = document.querySelector('.column2');
 
-// function imgMouseOver() {
-//     document.getElementsByClassName("name").style.visibility = "visible";
-// }
 
-function sparkle() {
-    const letter = document.getElementsByClassName("sparkle");
-    const sparkle = "*";
-    letter.replaceWith(sparkle);
-}
+let checked = 1;
+// let themeColor = document.querySelector('body').style.backgroundColor;
+
+
+document.querySelector('.toggleContainer #toggle').addEventListener('click', e => {
+    if (checked==1){
+        mainContent.style.display = e.target.checked ? 'block' : 'none';
+        secondaryContent.style.display = e.target.checked ? 'none' : 'block';
+        document.querySelector('html').style.backgroundColor = "var(--dark)";
+        checked=2;
+    } else {
+        mainContent.style.display = e.target.checked ? 'none' : 'block';
+        secondaryContent.style.display = e.target.checked ? 'block' : 'none';
+        document.querySelector('html').style.backgroundColor = "var(--light)";
+        checked=1;
+    }
+
+    if (document.querySelector('html').style.backgroundColor.equals("var--dark")) {
+        doc
+    }
+    
+})
